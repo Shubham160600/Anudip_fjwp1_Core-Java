e641308e76f2b75fafbcfc00405e980f3aad7068
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2023-12-31 13:19:12 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private String studentName;
+    private String collegeName;
+    private int studentID;
+
+    public static void main(String[] args) {
+        System.out.println("Successful");
+    }
+}
