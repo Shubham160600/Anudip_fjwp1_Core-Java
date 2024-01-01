128d86980811a40b476d6204cde9864bbea2248b
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-01 16:29:15 +0530

    Create OddEvenCounter.java

---
+++ Week-2 Session-2(More on Flow Control and parameter passing)/Assignment3/OddEvenCounter.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class OddEvenCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int oddCount = 0;
+        int evenCount = 0;
+
+        while (true) {
+            System.out.print("Enter a number (-1 to exit): ");
+            int num = scanner.nextInt();
+
+            if (num == -1) {
+                break;
+            }
+
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Total Even Numbers: " + evenCount);
+        System.out.println("Total Odd Numbers: " + oddCount);
+    }
+}
