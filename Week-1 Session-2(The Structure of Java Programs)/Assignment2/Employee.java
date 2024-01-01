1aada6bf352fb4bf1ba7c8ea611d646a6ae32385
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-01 16:15:22 +0530

    Add files via upload

---
+++ Week-1 Session-2(The Structure of Java Programs)/Assignment2/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    protected int id;
+    protected int age;
+    protected String name;
+    protected boolean isPermanent;
+
+    public static void main(String[] args) {
+        System.out.println("Successfully started");
+    }
+}
