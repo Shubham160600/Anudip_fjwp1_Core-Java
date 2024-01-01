816607f729c0cf6ec2af1f54ad82ff74f746938c
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-01 16:28:14 +0530

    Create CharacterPrinter.java

---
+++ Week-2 Session-1 (Flow Control)/Practice/Assignment2/CharacterPrinter.java
@@ -0,0 +1,25 @@
+public class CharacterPrinter {
+    public static void main(String[] args) {
+        char character = 'C';
+
+        switch (character) {
+            case 'A':
+                System.out.println("A for Apple");
+                break;
+            case 'B':
+                System.out.println("B for Boy");
+                break;
+            case 'C':
+                System.out.println("C for Cat");
+                break;
+            case 'D':
+                System.out.println("D for Dog");
+                break;
+            case 'E':
+                System.out.println("E for Elephant");
+                break;
+            default:
+                System.out.println("Unknown character");
+        }
+    }
+}
