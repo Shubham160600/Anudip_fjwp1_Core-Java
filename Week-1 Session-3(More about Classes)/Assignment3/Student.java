b93b222ab22c9062df73e372e832bb9ecd48a580
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-01 16:24:38 +0530

    Create Student.java

---
+++ Week-1 Session-3(More about Classes)/Assignment3/Student.java
@@ -0,0 +1,50 @@
+//package Student_Java_Bean_Class;
+
+public class Student {
+    private String name;
+    private int age;
+    private String department;
+
+    public Student(String name, int age, String department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    // Getter and Setter methods
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the Student class and use getter and setter methods
+        Student student = new Student("Shubham Saurabh", 23, "Computer Science");
+
+        // Display student details
+        System.out.println("Student Details:");
+        System.out.println("Name: " + student.getName());
+        System.out.println("Age: " + student.getAge());
+        System.out.println("Department: " + student.getDepartment());
+    }
+}
