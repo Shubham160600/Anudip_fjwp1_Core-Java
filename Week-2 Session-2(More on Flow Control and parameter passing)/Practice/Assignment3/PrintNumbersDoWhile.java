6be11f1b5487fa4bfdac3ce9bb1cc81c46207a70
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-01 16:29:35 +0530

    Create PrintNumbersDoWhile.java

---
+++ Week-2 Session-2(More on Flow Control and parameter passing)/Practice/Assignment3/PrintNumbersDoWhile.java
@@ -0,0 +1,10 @@
+public class PrintNumbersDoWhile {
+    public static void main(String[] args) {
+        int number = 300;
+
+        do {
+            System.out.print(number + " ");
+            number--;
+        } while (number >= 1);
+    }
+}
