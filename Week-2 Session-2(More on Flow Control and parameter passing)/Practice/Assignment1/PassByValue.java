c95487fe15a0872aacf63f5580e70265443d2525
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-01 16:25:04 +0530

    Create PassByValue.java

---
+++ Week-2 Session-2(More on Flow Control and parameter passing)/Practice/Assignment1/PassByValue.java
@@ -0,0 +1,17 @@
+public class PassByValue {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println("Before calling the method: " + num);
+
+        // Call a method that increments the value
+        incrementValue(num);
+
+        System.out.println("After calling the method: " + num);
+    }
+
+    // Method to increment the value (pass by value)
+    public static void incrementValue(int x) {
+        x++;
+        System.out.println("Inside the method: " + x);
+    }
+}
