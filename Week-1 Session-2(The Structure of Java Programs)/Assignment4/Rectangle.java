0d7e2d32e71adbcd2ba92644b572afe99b954a61
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-01 16:31:51 +0530

    Create Rectangle.java

---
+++ Week-1 Session-2(The Structure of Java Programs)/Assignment4/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+    private double width;
+    private double height;
+
+    enum Color {
+        RED, GREEN, BLUE
+    }
+
+    private Color boxColor;
+
+    public static void main(String[] args) {
+        System.out.println(Color.BLUE);
+    }
+}
