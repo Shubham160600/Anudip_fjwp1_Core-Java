8a675b13c67af1982fd73b92bf6fcd68d0a2d03a
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-01 16:31:37 +0530

    Create Person.java

---
+++ Week-1 Session-2(The Structure of Java Programs)/Assignment3/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    String name = "Shubham Saurabh";
+    int age = 23;
+    int salary;
+
+    public static void main(String[] args) {
+        System.out.println("Test Successful");
+    }
+}
