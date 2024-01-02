70f0a4727e22246ae54a76535f961cc9d6da0ffb
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:19:30 +0530

    Update Student.java

--- Week-1 Session-2(The Structure of Java Programs)/Assignment1/Student.java
+++ Week-1 Session-2(The Structure of Java Programs)/Assignment1/Student.java
@@ -1,3 +1,11 @@
+/* 
+● Create a class Student in Student.java then add member variables studentName, collegeName of type String.
+● Add a member variable studentID of type int.
+● Make all the member variables as private.
+● Add a main method. And print a message “Successful”.
+● Compile the class.
+● Run the class (Follow Coding convention). */
+
 public class Student {
     private String studentName;
     private String collegeName;
