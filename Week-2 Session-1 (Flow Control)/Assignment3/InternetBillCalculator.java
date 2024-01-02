aa288c1dd662e06b5e91633aa6c7c6bd0e5b7c11
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:45:56 +0530

    Update InternetBillCalculator.java

--- Week-2 Session-1 (Flow Control)/Assignment3/InternetBillCalculator.java
+++ Week-2 Session-1 (Flow Control)/Assignment3/InternetBillCalculator.java
@@ -1,3 +1,18 @@
+/* 
+Write a program which calculates the monthly bill amount for a
+internet subscriber based on the following logic:
+1) If the total data consumed is less than 10 GB then bill amount
+will be Rs. 300. (Basic charge)
+2) If the data consumed is between 10 GB and 30 GB then bill
+amount will be basic charge + 5* (Total GB consumed - 10).
+That means consumers will be charged Rs 5 for each
+additional GB consumed over 10 GB.
+3) If the consumer consumes more than 30 GB then
+The bill amount = 400 + 3 *(Total GB consumed - 30).
+That means the consumer has to pay additional Rs 3 for each
+GB above 30GB.
+Use if-else block to solve the problem. */
+
 import java.util.Scanner;
 
 public class InternetBillCalculator {
