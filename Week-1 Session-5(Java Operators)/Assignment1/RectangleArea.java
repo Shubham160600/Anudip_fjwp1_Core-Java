c1ae3dfc96a622431e49f4f01281aafe16a85ab9
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:36:26 +0530

    Update RectangleArea.java

--- Week-1 Session-5(Java Operators)/Assignment1/RectangleArea.java
+++ Week-1 Session-5(Java Operators)/Assignment1/RectangleArea.java
@@ -1,4 +1,16 @@
-//package saurabh;
+/* 
+Write a Java program that uses a method to calculate the area of a rectangle and
+compare them using Relational Operator
+Steps:
+● Create a class Rectangle.
+● The Rectangle class should have two attributes length and width of type int.
+● Create a constructor that accepts length and width as parameters.
+● Area should be calculated as length*area.
+● Instantiate two Rectangle classes with random values.
+● Compare the areas of the two rectangles using the Relational Operator.
+● If the first one is bigger than the second one, print “Rectangle1 > Rectangle2”.
+● If the first one is smaller print “Rectangle1 < Rectangle2”.
+● Otherwise print “They are equal”. */
 
 class Rectangle {
 	private int length;
