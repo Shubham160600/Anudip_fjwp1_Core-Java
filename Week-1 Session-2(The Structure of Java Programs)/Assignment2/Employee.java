72496465531aa71194b491c2f8222bf2ea68923c
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:19:45 +0530

    Update Employee.java

--- Week-1 Session-2(The Structure of Java Programs)/Assignment2/Employee.java
+++ Week-1 Session-2(The Structure of Java Programs)/Assignment2/Employee.java
@@ -1,3 +1,12 @@
+/* 
+● Create a new class Employee.
+● Add member variables: id and age of type int, name of type String and isPermanent of type boolean.
+● Now assign values 35.5 to age; See the error message.
+● How can you avoid this error? Correct the error by casting.
+● Make all the members protected.
+● Add a main method to it. Print message “Successfully started”.
+● Compile the class. */
+
 public class Employee {
     protected int id;
     protected int age;
