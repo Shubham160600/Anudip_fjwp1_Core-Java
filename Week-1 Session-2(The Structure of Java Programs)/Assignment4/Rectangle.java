98525f0eaaef8f0225bc2c133387e9ff046d9f62
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:19:53 +0530

    Update Rectangle.java

--- Week-1 Session-2(The Structure of Java Programs)/Assignment4/Rectangle.java
+++ Week-1 Session-2(The Structure of Java Programs)/Assignment4/Rectangle.java
@@ -1,3 +1,12 @@
+/* 
+● Create a class Rectangle.
+● Add a member variable width and height of type double.
+● Create an enum Color with values RED, GREEN, BLUE.
+● Create a member variable boxColor of type Color.
+● Add a main method.
+● In main method just print the enum Color.BLUE (You will notice that Java prints the enum name as it is).
+● Compile and run the class. */
+
 public class Rectangle {
     private double width;
     private double height;
