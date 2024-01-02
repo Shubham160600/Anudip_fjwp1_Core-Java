792338c5850e672c848ffa484999eb8fc9551310
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:45:47 +0530

    Update SimpleInterestCalculator.java

--- Week-2 Session-1 (Flow Control)/Assignment1/SimpleInterestCalculator.java
+++ Week-2 Session-1 (Flow Control)/Assignment1/SimpleInterestCalculator.java
@@ -1,3 +1,12 @@
+/* 
+Write a program to enter P,T (principal and time) and calculate
+simple interest. Use the Scanner class for taking inputs from the
+console. Users will enter the principal amount and time(in years).
+Create an if-else statement and modify the interest rate based on the
+principal amount. If the amount > 10000 then the interest rate is 10%. If the
+amount is between 10000 and 5000 then make the interest rate 8%. For
+any amount below 5000 the interest rate should be 5%. */
+
 import java.util.Scanner;
 
 public class SimpleInterestCalculator {
