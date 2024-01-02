04eb6564e62b886b26572f483281f3dacdb17e72
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:34:10 +0530

    Update BankName.java

--- Week-1 Session-4(OOPS)/Assignment3/BankName.java
+++ Week-1 Session-4(OOPS)/Assignment3/BankName.java
@@ -1,4 +1,14 @@
-//package Assignment3;
+/* 
+● Create an enum BankName.
+● Create a constructor of the enum which takes a parameter interestRate of type double.
+● Create the enum constant -ICICI (6.3), HDFC(5.8), SBI (6.0).
+● Now in the BankAccount class of previous exercise, modifythetype of bankName to enum BankName. Change constructor accordingly.
+● Modify the instantiation of a new Account accordingly. (Example: new
+BankAccount(12000, "Harry", BankName.SBI);
+● Print the bankName for each account.
+● Add a method which calculates total interest - calculateInterest(int numberOfYears). The interest will be calculated basedoninterest 
+rate and available balance. Print total interest. [ Hints: double totalInterest = bankName.interestRate* numberOfYears * accountBalance;].
+ */
 
 public enum BankName {
     ICICI(6.3),
