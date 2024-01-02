ed3ff4d8b0a182a65fec38eda536c895c9c985e8
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:19:49 +0530

    Update Person.java

--- Week-1 Session-2(The Structure of Java Programs)/Assignment3/Person.java
+++ Week-1 Session-2(The Structure of Java Programs)/Assignment3/Person.java
@@ -1,3 +1,12 @@
+/* 
+● Create a class Person.
+● Add member variables name as String, age and salary as int.
+● Initialize the member variable along with declaration.
+● Now put the previous Person class in a package com.anudip.learning.
+● Add a main method. Add a print message “Test Successful”.
+● Run the class after compilation.
+● Modify the classpaths to see the error messages on the console. */
+
 public class Person {
     String name = "Shubham Saurabh";
     int age = 23;
