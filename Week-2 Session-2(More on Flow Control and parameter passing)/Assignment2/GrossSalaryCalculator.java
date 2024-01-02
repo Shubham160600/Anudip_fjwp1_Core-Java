c102293b836db71c6abe67363325ccfda3e4509f
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:46:12 +0530

    Update GrossSalaryCalculator.java

--- Week-2 Session-2(More on Flow Control and parameter passing)/Assignment2/GrossSalaryCalculator.java
+++ Week-2 Session-2(More on Flow Control and parameter passing)/Assignment2/GrossSalaryCalculator.java
@@ -1,3 +1,13 @@
+/* 
+Write a program to calculate the gross salary of a group of
+employees. Basic salary should be taken from the user.
+If the basic salary is greater than 15000 ,HRA=20% and DA=60% will be
+given, else HRA=3000 and DA 70% will be given to the employee.
+Note:Input of basic salary will be taken from the keyboard. After calculating
+the salary of one employee, the program will ask for the user's choice as
+int. If “-1” is entered then the loop will continue and the loop will exit for
+other int inputs. */
+
 import java.util.Scanner;
 
 public class GrossSalaryCalculator {
