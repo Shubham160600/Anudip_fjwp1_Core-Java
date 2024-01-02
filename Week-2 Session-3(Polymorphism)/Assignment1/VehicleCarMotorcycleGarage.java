bb80a36dc0bcb842d3fcbd4f229175f542923a5b
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 16:32:37 +0530

    Update VehicleCarMotorcycleGarage.java

--- Week-2 Session-3(Polymorphism)/Assignment1/VehicleCarMotorcycleGarage.java
+++ Week-2 Session-3(Polymorphism)/Assignment1/VehicleCarMotorcycleGarage.java
@@ -1,4 +1,11 @@
-package Shubh;
+/* 
+Create a base class called Vehicle with the following methods:
+● void start(): This method should print "Vehicle started." Create two subclasses of Vehicle called Car and Motorcycle. Override the start() method in each 
+subclass to provide a specific implementation:
+● Car: Print "Car started."
+● Motorcycle: Print "Motorcycle started." Create a class called Garage with a method named serviceVehicle(Vehicle vehicle). Inside this method, call the start() 
+method of the provided vehicle object and print "Vehicle serviced." In the Main class, create instances of Car and Motorcycle. Create an instance of the Garage 
+class. Call the serviceVehicle() method of the Garage class with instances of both Car and Motorcycle. */
 
 class Vehicle {
     void start() {
