6e87b862fce6da34287fcbaff4c2d245e4e0e26c
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:46:29 +0530

    Update OddEvenCounter.java

--- Week-2 Session-2(More on Flow Control and parameter passing)/Assignment3/OddEvenCounter.java
+++ Week-2 Session-2(More on Flow Control and parameter passing)/Assignment3/OddEvenCounter.java
@@ -1,3 +1,8 @@
+/* 
+Write a program to count and print the total number of odd and even
+numbers from user inputs. Program will ask for user inputs in a loop. Loop
+will terminate if -1 is entered as input. */
+
 import java.util.Scanner;
 
 public class OddEvenCounter {
