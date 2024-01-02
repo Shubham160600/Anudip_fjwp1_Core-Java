b433e52d8f67cc5c54eb2ee89069711601a167dd
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-02 10:20:41 +0530

    Update Car.java

--- Week-1 Session-3(More about Classes)/Assignment1/Car.java
+++ Week-1 Session-3(More about Classes)/Assignment1/Car.java
@@ -1,4 +1,9 @@
-package shubham;
+/* 
+● Write a Java program named Car.
+● The Car class should have the following attributes: make (String), model (String) , year (short) , and price(int).
+● The car class should have a constructor that takes all the attributes.
+● Add a main method to instantiate car objects.
+● The program should allow the user to create and display objects of each Car Class. */
 
 import java.util.Scanner;
 
