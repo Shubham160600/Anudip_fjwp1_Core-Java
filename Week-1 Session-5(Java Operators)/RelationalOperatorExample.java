09712495f41a93764b67667b6536b06233368056
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 09:35:05 +0530

    Create RelationalOperatorExample.java

---
+++ Week-1 Session-5(Java Operators)/RelationalOperatorExample.java
@@ -0,0 +1,22 @@
+package abc;
+
+public class RelationalOperatorExample {
+public static void main(String[] args) {
+int a = 9, b = 15;
+// value of a and b
+System.out.println("a =" + a + " and b=" + b);
+// == operator
+// Note here additional parenthesis is used as operators have precedence
+System.out.println( "(a == b) is " + (a == b)); // false
+// != operator
+System.out.println("(a != b) is " + (a != b)); // true
+// > operator
+System.out.println("(a > b) is " + (a > b)); // false
+// < operator
+System.out.println("(a < b) is " + (a < b)); // true
+// >= operator
+System.out.println( "(a >= b) is " + (a >= b)) ; // false
+// <= operator
+System.out.println(a <= b); // true
+}
+}
