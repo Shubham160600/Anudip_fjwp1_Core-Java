cd647fa1755d218cfce4219535ddbe91b9f37193
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 09:48:57 +0530

    Create DoLoopExample.java

---
+++ Week-2 Session-1 (Flow Control)/DoLoopExample.java
@@ -0,0 +1,16 @@
+package shubham;
+
+/**
+*
+* A simple program to explain Do loop in Java
+* */
+public class DoLoopExample {
+public static void main(String[] args) {
+int count = 0;
+while (count < 5) {
+System.out.println("Count is: " + count);
+count++;
+}
+System.out.println("The value of count after the loop is " + count);
+}
+}
