eb02b9fe87a7d19320e3301771731a507c4af47e
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 08:16:56 +0530

    Create Rectangle.java

---
+++ Week-1 Session-4(OOPS)/Rectangle.java
@@ -0,0 +1,17 @@
+package abc;
+
+class Rectangle implements Shape {
+    // Fields: length, width
+    
+    // Constructor
+    public Rectangle(double length, double width) {
+        // Initialize fields
+    }
+    
+    // Implementation of the draw method
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle...");
+        // Additional code for drawing a rectangle
+    }
+}
