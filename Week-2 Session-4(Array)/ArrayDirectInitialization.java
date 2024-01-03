dcfa1702be644ee3064f3f664b2f4476b71a17af
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 12:15:36 +0530

    Create ArrayDirectInitialization.java

---
+++ Week-2 Session-4(Array)/ArrayDirectInitialization.java
@@ -0,0 +1,12 @@
+package shubham;
+
+public class ArrayDirectInitialization {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("The elements are:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
