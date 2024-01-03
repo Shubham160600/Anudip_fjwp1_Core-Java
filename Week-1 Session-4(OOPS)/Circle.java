9a4f9533efe5a9fcc6e41258cec3b16b3d0d8df8
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 08:16:27 +0530

    Create Circle.java

---
+++ Week-1 Session-4(OOPS)/Circle.java
@@ -0,0 +1,17 @@
+package abc;
+
+class Circle implements Shape {
+    // Fields: radius
+    
+    // Constructor
+    public Circle(double radius) {
+        // Initialize fields
+    }
+    
+    // Implementation of the draw method
+    @Override
+    public void draw() {
+        System.out.println("Drawing Circle...");
+        // Additional code for drawing a circle
+    }
+}
