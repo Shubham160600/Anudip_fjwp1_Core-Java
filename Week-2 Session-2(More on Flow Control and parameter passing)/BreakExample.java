947a27b6a494ef262a9c8d5fbf81ac597db64ad4
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 11:10:01 +0530

    Create BreakExample.java

---
+++ Week-2 Session-2(More on Flow Control and parameter passing)/BreakExample.java
@@ -0,0 +1,17 @@
+package shubham;
+
+public class BreakExample {
+public static void main(String[] args) {
+//initiating while loop
+int a=1;
+while(a<=10){
+if(a==5){
+//using break statement
+a++;
+break;//it will break the while loop
+}
+System.out.println(a);
+a++;
+}
+}
+}
