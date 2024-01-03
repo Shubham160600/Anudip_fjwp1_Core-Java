fb8d512529bc6d406755ae7125e1318befb47345
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 11:36:48 +0530

    Create Employee1.java

---
+++ Week-2 Session-3(Polymorphism)/Employee1.java
@@ -0,0 +1,10 @@
+package shubham;
+
+public class Employee1 {
+public Employee1() {
+System.out.println("Creating an employee in Constructor");
+}
+public static void main(String[] args) {
+Employee1 emp = new Employee1();
+}
+}
