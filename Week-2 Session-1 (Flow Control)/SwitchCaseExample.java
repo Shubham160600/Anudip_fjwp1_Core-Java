03447c81deabf89dee37c6931d67d43395508520
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 09:49:36 +0530

    Create SwitchCaseExample.java

---
+++ Week-2 Session-1 (Flow Control)/SwitchCaseExample.java
@@ -0,0 +1,33 @@
+package shubham;
+
+public class SwitchCaseExample {
+public static void main(String[] args) {
+int dayOfWeek = 3;
+switch (dayOfWeek) {
+case 1:
+System.out.println("Monday");
+break;
+case 2:
+System.out.println("Tuesday");
+break;
+case 3:
+System.out.println("Wednesday");
+break;
+case 4:
+System.out.println("Thursday");
+break;
+case 5:
+System.out.println("Friday");
+break;
+case 6:
+System.out.println("Saturday");
+break;
+case 7:
+System.out.println("Sunday");
+break;
+default:
+System.out.println("Invalid day");
+break;
+}
+}
+}
