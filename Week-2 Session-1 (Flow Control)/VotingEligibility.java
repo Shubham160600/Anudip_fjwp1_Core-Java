fa589dd23e753d6c3226136ba074d6f373e9f9d1
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 09:49:54 +0530

    Create VotingEligibility.java

---
+++ Week-2 Session-1 (Flow Control)/VotingEligibility.java
@@ -0,0 +1,22 @@
+package shubham;
+
+public class VotingEligibility {
+public static void main(String[] args) {
+//age
+int age = 25;
+//Is Indian citizen?
+boolean isIndianCitizen = true;
+// Check voting eligibility
+if (isIndianCitizen) {
+if (age>=18) {
+System.out.println("You are eligible to vote.");
+}
+else {
+System.out.println("You must be 18 years old or above to vote.");
+}
+}
+else {
+System.out.println("Only citizens are eligible to vote.");
+}
+}
+}
