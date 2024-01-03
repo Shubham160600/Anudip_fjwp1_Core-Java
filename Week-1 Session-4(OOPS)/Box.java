021b1d6a63113088ea5ebf3a2689434a0613b280
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 08:16:19 +0530

    Create Box.java

---
+++ Week-1 Session-4(OOPS)/Box.java
@@ -0,0 +1,17 @@
+package abc;
+
+class Box implements Shape {
+    // Fields: length, width, height
+    
+    // Constructor
+    public Box(double length, double width, double height) {
+        // Initialize fields
+    }
+    
+    // Implementation of the draw method
+    @Override
+    public void draw() {
+        System.out.println("Drawing Box...");
+        // Additional code for drawing a box
+    }
+}
