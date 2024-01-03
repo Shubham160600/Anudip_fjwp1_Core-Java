144c5c5f32bd8e178ac494e21a3fff7d30e08a84
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 09:34:51 +0530

    Create InstanceofExample.java

---
+++ Week-1 Session-5(Java Operators)/InstanceofExample.java
@@ -0,0 +1,11 @@
+package abc;
+
+public class InstanceofExample {
+public static void main(String[] args) {
+String str = "Anudip";
+boolean result;
+// Verifies if it is an instance of String class
+result = str instanceof String;
+System.out.println("Is str an instance of String? " + result);
+}
+}
