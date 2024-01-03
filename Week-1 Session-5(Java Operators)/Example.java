e499ab5157775c2c439885e1b734db3fe8616172
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 09:34:37 +0530

    Create Example.java

---
+++ Week-1 Session-5(Java Operators)/Example.java
@@ -0,0 +1,27 @@
+package shubham;
+
+public class Example {
+public static void main(String[] args) {
+// Create variables
+int a = 3;
+int var;
+System.out.println("a = " + a);
+// assign value using =
+var = a;
+System.out.println("var using = " + var);
+// assign value using +=
+var += a;
+System.out.println("var using += " + var);
+var -= a;
+System.out.println ( "var using -= " + var);
+// assign value using *=
+var *= a;
+System.out.println("var using *= " + var);
+// assign value using /=
+var /= a ;
+System.out.println("var using /= " + var);
+// assign value using %=
+var %= a;
+System.out.println("var using %= " + var);
+}
+}
