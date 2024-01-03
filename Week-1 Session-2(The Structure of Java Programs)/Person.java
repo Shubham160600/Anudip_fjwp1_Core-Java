86a172324cb4ce450fb1b5a269381e6c3c3a0792
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 07:16:45 +0530

    Create Person.java

---
+++ Week-1 Session-2(The Structure of Java Programs)/Person.java
@@ -0,0 +1,10 @@
+package com.anudip.example;
+
+import java.util.*;
+public class Person {
+int age = 25;
+int salary = 50000;
+public static void main(String[] args) {
+System.out.println ("This is Person Class");
+}
+}
