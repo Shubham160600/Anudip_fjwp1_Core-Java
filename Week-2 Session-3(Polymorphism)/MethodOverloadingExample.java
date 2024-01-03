d39c6f29b66802dd1b340cf64eb727ac49662955
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 11:48:11 +0530

    Create MethodOverloadingExample.java

---
+++ Week-2 Session-3(Polymorphism)/MethodOverloadingExample.java
@@ -0,0 +1,30 @@
+/*
+* Example of Method Overloading
+*
+* Figure class has two methods with the same name
+* getArea() but they take different numberof parameters.
+* Return types are also different in this example.
+* However, return type can be same also for method overloading
+*/
+
+package shubham;
+
+public class MethodOverloadingExample {
+    public static void main(String[] args) {
+        Figure obj = new Figure();
+        System.out.println("Area of Rectangle: " + obj.getArea(7.25, 6));
+        System.out.println("Area of Square: " + obj.getArea(5));
+    }
+}
+
+class Figure {
+    // Return Area of a Rectangular Figure
+    double getArea(double l, int b) { //two double parameters, return type double
+        return (l * b);
+    }
+
+    // Return area of a square
+    float getArea(float s) {
+        return (s * s);
+    }
+}
