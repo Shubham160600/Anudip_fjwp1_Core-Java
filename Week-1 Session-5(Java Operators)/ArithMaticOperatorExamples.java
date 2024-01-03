86c7e9f7c207e073669ade0c25c9d77de9109e5c
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-03 08:32:38 +0530

    Create ArithMaticOperatorExamples.java

---
+++ Week-1 Session-5(Java Operators)/ArithMaticOperatorExamples.java
@@ -0,0 +1,18 @@
+package abc;
+
+public class ArithMaticOperatorExamples {
+public static void main(String[] args) {
+int n1 = 12, n2 = 5;
+System.out.println("n1=" + n1 + ", n2=" + n2);
+// addition operator
+System.out.println("Addition: n1 + n2= " + (n1 + n2));
+// subtraction operator
+System.out.println("Subtraction: n1 - n2 = " + (n1 - n2));
+// multiplication operator
+System.out.println("Multiplication: n1 * n2 = " + (n1 * n2));
+// division operator
+System.out.println("Division: n1 / n2 = " + (n1 / n2));
+// modulo operator
+System.out.println("Modulo: n1 % n2 = " + (n1 % n2));
+}
+}
