2ed399f391163bd06136c9ccac34a0c725d77354
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 17:58:55 +0530

    Update Employee.java

--- Week-1 Session-3(More about Classes)/Employee.java
+++ Week-1 Session-3(More about Classes)/Employee.java
@@ -1,35 +1,47 @@
 package saurabh;
 
 public class Employee {
-int id;
-String name;
-boolean isPermanent;
-public int getId() {
-return id;
-}
-public void setId(int id) {
-this.id = id;
-}
-public String getName() {
-return name;
-}
-public void setName(String name) {
-this.name = name;
-}
-public boolean isPermanent() {
-return isPermanent;
-}
-public void setPermanent(boolean isPermanent) {
-this.isPermanent = isPermanent;
-}
-// No constructor is explicitly defined
-public static void main(String[] args) {
-// We are creating an instance without
-// creating any constructor.
-// Because Java compiler provides default constructor.
-Employee emp = new Employee ();
-//Accessing the method of the instance.
-emp.setName("John");
-System.out.println(emp.getName());
-}
+    // Instance variables
+    int id;
+    String name;
+    boolean isPermanent;
+
+    // Getter and setter methods for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and setter methods for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and setter methods for isPermanent
+    public boolean isPermanent() {
+        return isPermanent;
+    }
+
+    public void setPermanent(boolean isPermanent) {
+        this.isPermanent = isPermanent;
+    }
+
+    // No constructor is explicitly defined, so a default constructor is provided by the compiler
+
+    public static void main(String[] args) {
+        // Creating an instance without explicitly creating any constructor
+        // Because Java compiler provides a default constructor.
+        Employee emp = new Employee();
+
+        // Accessing the method of the instance.
+        emp.setName("John");
+        System.out.println(emp.getName());
+    }
 }
