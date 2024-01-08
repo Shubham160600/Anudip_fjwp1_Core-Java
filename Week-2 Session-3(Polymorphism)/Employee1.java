7341a35bf7dad6f1b575daecd5ec589f9cdf1f82
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:36 +0530

    Update Employee1.java

--- Week-2 Session-3(Polymorphism)/Employee1.java
+++ Week-2 Session-3(Polymorphism)/Employee1.java
@@ -1,10 +1,14 @@
 package shubham;
 
 public class Employee1 {
-public Employee1() {
-System.out.println("Creating an employee in Constructor");
-}
-public static void main(String[] args) {
-Employee1 emp = new Employee1();
-}
+    // Constructor
+    public Employee1() {
+        System.out.println("Creating an employee in Constructor");
+    }
+
+    // Main method to test the Employee1 class
+    public static void main(String[] args) {
+        // Creating an instance of Employee1
+        Employee1 emp = new Employee1();
+    }
 }
