42a99a3f1db004929718a90b282bc89a709f1e9c
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:04:42 +0530

    Update Example.java

--- Week-1 Session-5(Java Operators)/Example.java
+++ Week-1 Session-5(Java Operators)/Example.java
@@ -1,27 +1,36 @@
 package shubham;
 
 public class Example {
-public static void main(String[] args) {
-// Create variables
-int a = 3;
-int var;
-System.out.println("a = " + a);
-// assign value using =
-var = a;
-System.out.println("var using = " + var);
-// assign value using +=
-var += a;
-System.out.println("var using += " + var);
-var -= a;
-System.out.println ( "var using -= " + var);
-// assign value using *=
-var *= a;
-System.out.println("var using *= " + var);
-// assign value using /=
-var /= a ;
-System.out.println("var using /= " + var);
-// assign value using %=
-var %= a;
-System.out.println("var using %= " + var);
-}
+    public static void main(String[] args) {
+        // Create variables
+        int a = 3;
+        int var;
+
+        // Print the initial value of 'a'
+        System.out.println("a = " + a);
+
+        // Assign value using =
+        var = a;
+        System.out.println("var using = " + var);
+
+        // Assign value using +=
+        var += a;
+        System.out.println("var using += " + var);
+
+        // Assign value using -=
+        var -= a;
+        System.out.println("var using -= " + var);
+
+        // Assign value using *=
+        var *= a;
+        System.out.println("var using *= " + var);
+
+        // Assign value using /=
+        var /= a;
+        System.out.println("var using /= " + var);
+
+        // Assign value using %=
+        var %= a;
+        System.out.println("var using %= " + var);
+    }
 }
