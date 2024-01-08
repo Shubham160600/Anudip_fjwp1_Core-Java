5bfc323915aac3916ba74b12fbff18dc7eedb84b
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:56 +0530

    Update SwitchCaseWithMultipleConstants.java

--- Week-2 Session-1 (Flow Control)/SwitchCaseWithMultipleConstants.java
+++ Week-2 Session-1 (Flow Control)/SwitchCaseWithMultipleConstants.java
@@ -1,13 +1,15 @@
 package shubham;
 
 public class SwitchCaseWithMultipleConstants {
-public static void main(String[] args) {
-int month =14;
-switch (month) {
-case 1, 3, 5, 7, 8, 10, 12 -> System.out.println("this month has 31 days");
-case 4, 6, 9 -> System.out.println("this month has 30 days");
-case 2 -> System.out.println("February can have 28 or 29 days");
-default -> System.out.println("invalid month");
-}
-}
+    public static void main(String[] args) {
+        int month = 14;
+
+        // Switch expression with multiple constants in each case label
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> System.out.println("This month has 31 days");
+            case 4, 6, 9 -> System.out.println("This month has 30 days");
+            case 2 -> System.out.println("February can have 28 or 29 days");
+            default -> System.out.println("Invalid month");
+        }
+    }
 }
