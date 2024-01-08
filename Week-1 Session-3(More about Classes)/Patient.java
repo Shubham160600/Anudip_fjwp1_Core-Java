661a59264eb65de53e5479cfda6cc6e53704e8f5
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 17:58:21 +0530

    Update Patient.java

--- Week-1 Session-3(More about Classes)/Patient.java
+++ Week-1 Session-3(More about Classes)/Patient.java
@@ -1,44 +1,51 @@
 package shubham;
 
 public class Patient {
-private String name;
-private int age;
-private double height;
-// User-defined no-argument constructor
-public Patient() {
-// Member variables are assigned
-// fixed values as no values were
-// passed as parameters
-//
-this.name = "Unknown";
-this.age = 10;
-height =168.5;
-}
-// Rest of the getters and setters
-public static void main(String[] args) {
-	// Instantiation using new
-	Patient p = new Patient ();
-	p.setAge(35);
-	p.setHeight(159.5);
-	p.setName("John Doe");
-	System.out.println("Patient Name is " + p.getName());
-	}
-	public String getName() {
-	return name;
-	}
-	public void setName(String name) {
-	this.name = name;
-	}
-	public int getAge() {
-	return age;
-	}
-	public void setAge(int age) {
-	this.age = age;
-	}
-	public double getHeight() {
-	return height;
-	}
-	public void setHeight(double height) {
-	this.height = height;
-	}
+    private String name;
+    private int age;
+    private double height;
+
+    // User-defined no-argument constructor
+    public Patient() {
+        // Member variables are assigned fixed values as no values were
+        // passed as parameters
+        this.name = "Unknown";
+        this.age = 10;
+        height = 168.5;
+    }
+
+    // Rest of the getters and setters
+
+    public static void main(String[] args) {
+        // Instantiation using new
+        Patient p = new Patient();
+        p.setAge(35);
+        p.setHeight(159.5);
+        p.setName("John Doe");
+        System.out.println("Patient Name is " + p.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
