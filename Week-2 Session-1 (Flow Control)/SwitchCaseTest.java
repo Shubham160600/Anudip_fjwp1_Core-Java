fc443c948f10671b0a6a3fda279ca97a036f2bd0
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:31:00 +0530

    Update SwitchCaseTest.java

--- Week-2 Session-1 (Flow Control)/SwitchCaseTest.java
+++ Week-2 Session-1 (Flow Control)/SwitchCaseTest.java
@@ -1,20 +1,22 @@
 package shubham;
 
 public class SwitchCaseTest {
-public static void main(String[] args) {
-int month = 6;
-switch (month) {
-case 1, 3, 5, 7, 8, 10, 12 : 
-	System.out.println("this month has 31 days"); 
-break;
-case 4, 6, 9 : 
-	System.out.println("This month has 30 days");
-break;
-case 2 : 
-	System.out.println("February can have 28 or 29 days");
-	break;
-default : 
-	System.out.println("Invalid month");
-}
-}
+	public static void main(String[] args) {
+		int month = 6;
+
+		// Switch statement to determine the number of days in a month
+		switch (month) {
+			case 1, 3, 5, 7, 8, 10, 12:
+				System.out.println("This month has 31 days");
+				break;
+			case 4, 6, 9:
+				System.out.println("This month has 30 days");
+				break;
+			case 2:
+				System.out.println("February can have 28 or 29 days");
+				break;
+			default:
+				System.out.println("Invalid month");
+		}
+	}
 }
