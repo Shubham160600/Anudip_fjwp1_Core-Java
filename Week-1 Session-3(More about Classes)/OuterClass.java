26c6be4e0a275c04522853cae2beef24e7c81593
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 17:58:25 +0530

    Update OuterClass.java

--- Week-1 Session-3(More about Classes)/OuterClass.java
+++ Week-1 Session-3(More about Classes)/OuterClass.java
@@ -1,35 +1,43 @@
 package saurabh;
 
 public class OuterClass {
-	// Outer class members and methods
-	int nonStatic = 30;
-	static int staticVar=10;
-	// Static nested class
-	public static class StaticNestedClass {
-	// Static nested class members and methods
-	int innerVal =100;
-	void testMethod () {
-	// This is not allowed as we can not access
-	// non static member of outer class.
-	// nonStatic =30;
-	//Accessing static variable of outside class.
-	// This is allowed.
-	staticVar =40;
-	System.out.println(staticVar);
-	}
-	static void message() {
-	System.out.println("Static Method");
-	}
-	}
-	public static void main(String[] args) {
-	OuterClass.StaticNestedClass nestedObj = new
-	OuterClass.StaticNestedClass();
-	// Can access variable of nested class.
-	System.out.println(nestedObj.innerVal);
-	// Also we can call the methods of nested class;
-	nestedObj.testMethod();
-	//no need to create the instance of static nested class
-	// to call static method.
-	OuterClass.StaticNestedClass.message();
-	}
-	}
+    // Outer class members and methods
+    int nonStatic = 30;
+    static int staticVar = 10;
+
+    // Static nested class
+    public static class StaticNestedClass {
+        // Static nested class members and methods
+        int innerVal = 100;
+
+        void testMethod() {
+            // This is not allowed as we cannot access
+            // non-static member of the outer class.
+            // nonStatic = 30;
+
+            // Accessing static variable of the outer class.
+            // This is allowed.
+            staticVar = 40;
+            System.out.println(staticVar);
+        }
+
+        static void message() {
+            System.out.println("Static Method");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Creating an instance of the StaticNestedClass
+        OuterClass.StaticNestedClass nestedObj = new OuterClass.StaticNestedClass();
+
+        // Accessing the variable of the nested class.
+        System.out.println(nestedObj.innerVal);
+
+        // Calling the methods of the nested class.
+        nestedObj.testMethod();
+
+        // No need to create an instance of the static nested class
+        // to call the static method.
+        OuterClass.StaticNestedClass.message();
+    }
+}
