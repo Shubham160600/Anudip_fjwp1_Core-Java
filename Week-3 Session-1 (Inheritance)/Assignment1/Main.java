b9c4815dc3d9d992b0dad44611fb8f5c64f69dbe
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 20:01:16 +0530

    Update Main.java

--- Week-3 Session-1 (Inheritance)/Assignment1/Main.java
+++ Week-3 Session-1 (Inheritance)/Assignment1/Main.java
@@ -12,63 +12,63 @@ package shubham;
 
 //Vehicle class
 class Vehicle {
- String make;
- String model;
- int year;
- int maximumSpeed;
+    String make;
+    String model;
+    int year;
+    int maximumSpeed;
 
- // Constructor
- public Vehicle(String make, String model, int year, int maximumSpeed) {
-     this.make = make;
-     this.model = model;
-     this.year = year;
-     this.maximumSpeed = maximumSpeed;
- }
+    // Constructor
+    public Vehicle(String make, String model, int year, int maximumSpeed) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.maximumSpeed = maximumSpeed;
+    }
 
- // Drive method
- public void drive() {
-     System.out.println(make + " " + model + " is driving");
- }
+    // Drive method
+    public void drive() {
+        System.out.println(make + " " + model + " is driving");
+    }
 }
 
-//Car class, subclass of Vehicle
+// Car class, subclass of Vehicle
 class Car extends Vehicle {
- // Constructor calling super class constructor
- public Car(String make, String model, int year, int maximumSpeed) {
-     super(make, model, year, maximumSpeed);
- }
+    // Constructor calling super class constructor
+    public Car(String make, String model, int year, int maximumSpeed) {
+        super(make, model, year, maximumSpeed);
+    }
 
- // Override drive method
- @Override
- public void drive() {
-     System.out.println(make + " " + model + " Car is driving");
- }
+    // Override drive method
+    @Override
+    public void drive() {
+        System.out.println(make + " " + model + " Car is driving");
+    }
 }
 
-//Bike class, subclass of Vehicle
+// Bike class, subclass of Vehicle
 class Bike extends Vehicle {
- // Constructor calling super class constructor
- public Bike(String make, String model, int year, int maximumSpeed) {
-     super(make, model, year, maximumSpeed);
- }
+    // Constructor calling super class constructor
+    public Bike(String make, String model, int year, int maximumSpeed) {
+        super(make, model, year, maximumSpeed);
+    }
 
- // Override drive method
- @Override
- public void drive() {
-     System.out.println(make + " " + model + " Bike is driving");
- }
+    // Override drive method
+    @Override
+    public void drive() {
+        System.out.println(make + " " + model + " Bike is driving");
+    }
 }
 
 public class Main {
- public static void main(String[] args) {
-     // Instantiate Car
-     Car myCar = new Car("Toyota", "Camry", 2022, 200);
-     // Call drive method
-     myCar.drive();
+    public static void main(String[] args) {
+        // Instantiate Car
+        Car myCar = new Car("Toyota", "Camry", 2022, 200);
+        // Call drive method
+        myCar.drive();
 
-     // Instantiate Bike
-     Bike myBike = new Bike("Honda", "CBR", 2022, 150);
-     // Call drive method
-     myBike.drive();
- }
+        // Instantiate Bike
+        Bike myBike = new Bike("Honda", "CBR", 2022, 150);
+        // Call drive method
+        myBike.drive();
+    }
 }
