e6ffbc4c97548a3abb5eef5b59dccae0c6c97821
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:18 +0530

    Update LabelExample.java

--- Week-2 Session-2(More on Flow Control and parameter passing)/LabelExample.java
+++ Week-2 Session-2(More on Flow Control and parameter passing)/LabelExample.java
@@ -1,16 +1,19 @@
 package shubham;
 
 public class LabelExample {
-public static void main(String[] args) {
-outerLoop: // This is a labeled statement
-for (int i = 1; i <= 3; i++) {
-for (int j = 1; j <= 3; j++) {
-if (i * j > 5) {
-System.out.println("Exiting both loops.");
-break outerLoop; // Using the labeled statement with break to exit both loops
-}
-System.out.println("i=" + i + " j=" + " i*j= " + i * j);
-}
-}
-}
+    public static void main(String[] args) {
+        // Labeled statement 'outerLoop'
+        outerLoop: for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                // Checking if the product of i and j is greater than 5
+                if (i * j > 5) {
+                    System.out.println("Exiting both loops.");
+                    // Using the labeled statement with break to exit both loops
+                    break outerLoop;
+                }
+                // Printing the values of i, j, and the product of i and j
+                System.out.println("i=" + i + " j=" + j + " i*j=" + i * j);
+            }
+        }
+    }
 }
