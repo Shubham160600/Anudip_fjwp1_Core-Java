3d49825d3257df54071a6257d5575ad00ab87ae4
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:29 +0530

    Update StaticInitializationBlockTest.java

--- Week-2 Session-2(More on Flow Control and parameter passing)/StaticInitializationBlockTest.java
+++ Week-2 Session-2(More on Flow Control and parameter passing)/StaticInitializationBlockTest.java
@@ -1,23 +1,30 @@
 package shubham;
 
 public class StaticInitializationBlockTest {
-private static int count;
-// Static initialization block
-static {
-count = 0;
-System.out.println("Static initialization block executed.");
-}
-// Constructor
-public StaticInitializationBlockTest () {
-count++;
-System.out.println("Constructor executed.");
-}
-public static void displayCount() {
-System.out.println("Count: " + count);
-}
-public static void main(String[] args) {
-StaticInitializationBlockTest obj1 = new StaticInitializationBlockTest();
-StaticInitializationBlockTest obj2 = new StaticInitializationBlockTest();
-StaticInitializationBlockTest.displayCount();
-}
+    private static int count;
+
+    // Static initialization block
+    static {
+        count = 0;
+        System.out.println("Static initialization block executed.");
+    }
+
+    // Constructor
+    public StaticInitializationBlockTest() {
+        count++;
+        System.out.println("Constructor executed.");
+    }
+
+    public static void displayCount() {
+        System.out.println("Count: " + count);
+    }
+
+    public static void main(String[] args) {
+        // Creating two instances of StaticInitializationBlockTest
+        StaticInitializationBlockTest obj1 = new StaticInitializationBlockTest();
+        StaticInitializationBlockTest obj2 = new StaticInitializationBlockTest();
+
+        // Calling the static method to display the count
+        StaticInitializationBlockTest.displayCount();
+    }
 }
