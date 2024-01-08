c4ad1e762dac3eea77f521017fb2e2010855abe3
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:09:26 +0530

    Update LogicalOperatorExample.java

--- Week-1 Session-5(Java Operators)/LogicalOperatorExample.java
+++ Week-1 Session-5(Java Operators)/LogicalOperatorExample.java
@@ -1,17 +1,18 @@
 package abc;
 
 public class LogicalOperatorExample {
-public static void main(String[] args) {
-// && operator
-System.out.println((10 > 3) && (12 > 5)); // true
-System.out.println((10 > 3) && (12 < 5)); // false
-// || operator
-System.out.println((10 < 3) || (12 > 5)); // true
-System.out.println((10 > 3) || (12 < 5)); // true
-System.out.println((10 < 3) || (12 < 5)); // false
-// ! operator
-System.out.println(!(10 == 3)); // true
-System.out.println(!(10 > 3)); // false
-}
-}
+    public static void main(String[] args) {
+        // && operator
+        System.out.println((10 > 3) && (12 > 5)); // true
+        System.out.println((10 > 3) && (12 < 5)); // false
 
+        // || operator
+        System.out.println((10 < 3) || (12 > 5)); // true
+        System.out.println((10 > 3) || (12 < 5)); // true
+        System.out.println((10 < 3) || (12 < 5)); // false
+
+        // ! operator
+        System.out.println(!(10 == 3)); // true
+        System.out.println(!(10 > 3)); // false
+    }
+}
