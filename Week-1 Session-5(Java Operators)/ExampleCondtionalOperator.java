403931493b53971c68d2ef2c1b08f725a71e6f36
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:09:34 +0530

    Update ExampleCondtionalOperator.java

--- Week-1 Session-5(Java Operators)/ExampleCondtionalOperator.java
+++ Week-1 Session-5(Java Operators)/ExampleCondtionalOperator.java
@@ -1,18 +1,20 @@
 package abc;
 
 public class ExampleCondtionalOperator {
-public static void main(String[] args) {
-int daysInFebruary = 29;
-String result;
-// ternary operator
-result = (daysInFebruary == 28) ? "Not a leap year" : "Leap year";
-// Above statement is same as writing;
-// if (daysInFebruary == 28) {
-// result="Not a leap year";
-// }
-// else {
-// result="Leap year";
-// }
-System.out.println(result);
-}
+    public static void main(String[] args) {
+        int daysInFebruary = 29;
+        String result;
+
+        // Ternary conditional operator
+        result = (daysInFebruary == 28) ? "Not a leap year" : "Leap year";
+
+        // The above statement is equivalent to the following if-else block:
+        // if (daysInFebruary == 28) {
+        // result = "Not a leap year";
+        // } else {
+        // result = "Leap year";
+        // }
+
+        System.out.println(result);
+    }
 }
