6ba77df528d797bddfcc21e85c7df9bda9b46163
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:42:10 +0530

    Update MultidimensionalArray.java

--- Week-2 Session-5(Multi-dimensional array and Wrapper Classes)/MultidimensionalArray.java
+++ Week-2 Session-5(Multi-dimensional array and Wrapper Classes)/MultidimensionalArray.java
@@ -1,16 +1,17 @@
 package shubham;
 
 public class MultidimensionalArray {
-public static void main(String[] args) {
-// create a 2d array
-int[][] a = {
-{1, 2},
-{4, 5, 6, 9, 10},
-{7},
-};
-// calculate the length of each row
-System.out.println("Length of row 1: " + a[0].length);
-System.out.println("Length of row 2: " + a[1].length);
-System.out.println("Length of row 3: " + a[2].length);
-}
+    public static void main(String[] args) {
+        // Create a 2D array
+        int[][] a = {
+                { 1, 2 },
+                { 4, 5, 6, 9, 10 },
+                { 7 },
+        };
+
+        // Calculate the length of each row
+        System.out.println("Length of row 1: " + a[0].length);
+        System.out.println("Length of row 2: " + a[1].length);
+        System.out.println("Length of row 3: " + a[2].length);
+    }
 }
