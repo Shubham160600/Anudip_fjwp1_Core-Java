b22b4f7b27e8bbb04ce3b127b1f8f32367032a0b
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:38:19 +0530

    Update ArrayInitialization.java

--- Week-2 Session-4(Array)/ArrayInitialization.java
+++ Week-2 Session-4(Array)/ArrayInitialization.java
@@ -1,19 +1,20 @@
 package shubham;
 
 public class ArrayInitialization {
-public static void main(String[] args) {
-int[] arr=new int[10];
-System.out.println("Printing the default values for all elements of the int array:");
-for(int i=0;i<arr.length;i++)
-{
-System.out.print(arr[i] + " ");
-}
-System.out.println();
-boolean[] boolArr = new boolean[10];
-System.out.println("Printing the default values for all elements of the boolean array:");
-for(int i=0;i<arr.length;i++)
-{
-System.out.print(boolArr[i] + " ");
-}
-}
+    public static void main(String[] args) {
+        // Initializing an int array with default values (0 for int)
+        int[] arr = new int[10];
+        System.out.println("Printing the default values for all elements of the int array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // Initializing a boolean array with default values (false for boolean)
+        boolean[] boolArr = new boolean[10];
+        System.out.println("Printing the default values for all elements of the boolean array:");
+        for (int i = 0; i < boolArr.length; i++) {
+            System.out.print(boolArr[i] + " ");
+        }
+    }
 }
