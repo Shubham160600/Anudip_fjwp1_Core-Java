0faef0753bd9c87195a0ffd381af35279a633d34
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:44 +0530

    Update Person.java

--- Week-2 Session-3(Polymorphism)/Person.java
+++ Week-2 Session-3(Polymorphism)/Person.java
@@ -1,18 +1,21 @@
 package shubham;
 
 public class Person {
-private String name="Shubham";
-private int age=23;
-// Default constructor (automatically generated)
-// It will look like following
-// public Person() {
-// // No specific initialization
-// }
-public void displayInfo() {
-System.out.println("Name: " + name + ", Age: " + age);
-}
-public static void main(String[] args) {
-Person person = new Person(); // Calls the default constructor
-person.displayInfo();
-}
+    private String name = "Shubham";
+    private int age = 23;
+
+    // Default constructor (automatically generated)
+    // It will look like the following
+    // public Person() {
+    // // No specific initialization
+    // }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person(); // Calls the default constructor
+        person.displayInfo();
+    }
 }
