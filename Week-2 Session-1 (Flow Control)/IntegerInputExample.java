2715a7cd4afeff9d50e9523ac15fd6e589652813
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:15:01 +0530

    Update IntegerInputExample.java

--- Week-2 Session-1 (Flow Control)/IntegerInputExample.java
+++ Week-2 Session-1 (Flow Control)/IntegerInputExample.java
@@ -1,12 +1,22 @@
 package shubham;
 
 import java.util.Scanner;
+
 public class IntegerInputExample {
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-System.out.print("Enter an integer: ");
-int num = scanner.nextInt();
-System.out.println("You entered: " + num);
-scanner.close();
-}
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter an integer
+        System.out.print("Enter an integer: ");
+
+        // Read the user input as an integer
+        int num = scanner.nextInt();
+
+        // Display the entered integer
+        System.out.println("You entered: " + num);
+
+        // Close the Scanner to release system resources
+        scanner.close();
+    }
 }
