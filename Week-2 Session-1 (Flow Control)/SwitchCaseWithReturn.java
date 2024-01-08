795ba75bb70b05f5ea39371ffd1ec1b6f07a5e0c
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:52 +0530

    Update SwitchCaseWithReturn.java

--- Week-2 Session-1 (Flow Control)/SwitchCaseWithReturn.java
+++ Week-2 Session-1 (Flow Control)/SwitchCaseWithReturn.java
@@ -1,16 +1,19 @@
 package shubham;
 
 public class SwitchCaseWithReturn {
-static int getDays (int month) {
-return switch (month) {
-case 1, 3, 5, 7, 8, 10, 12 -> 31;
-case 4, 6, 9 -> 30;
-case 2 -> 28;
-default -> 0;
-};
-}
-public static void main(String[] args) {
-int month =3; //March
-System.out.println("Number of days in March=" + getDays(month));
-}
+    // Method to get the number of days in a month using a switch expression
+    static int getDays(int month) {
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9 -> 30;
+            case 2 -> 28;
+            default -> 0;
+        };
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int month = 3; // March
+        System.out.println("Number of days in March = " + getDays(month));
+    }
 }
