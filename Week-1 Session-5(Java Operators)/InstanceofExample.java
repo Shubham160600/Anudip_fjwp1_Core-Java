ae3c4ea07b4aa1484a81f4f43c3b33c39895871e
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:09:30 +0530

    Update InstanceofExample.java

--- Week-1 Session-5(Java Operators)/InstanceofExample.java
+++ Week-1 Session-5(Java Operators)/InstanceofExample.java
@@ -1,11 +1,13 @@
 package abc;
 
 public class InstanceofExample {
-public static void main(String[] args) {
-String str = "Anudip";
-boolean result;
-// Verifies if it is an instance of String class
-result = str instanceof String;
-System.out.println("Is str an instance of String? " + result);
-}
+    public static void main(String[] args) {
+        // Create a String object
+        String str = "Anudip";
+        boolean result;
+
+        // Verifies if 'str' is an instance of String class
+        result = str instanceof String;
+        System.out.println("Is str an instance of String? " + result);
+    }
 }
