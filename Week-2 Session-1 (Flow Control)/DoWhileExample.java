c983b39d6e2e007099c838c48ff633832c3f7d8f
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:15:04 +0530

    Update DoWhileExample.java

--- Week-2 Session-1 (Flow Control)/DoWhileExample.java
+++ Week-2 Session-1 (Flow Control)/DoWhileExample.java
@@ -4,13 +4,16 @@ package shubham;
 * Example to explain Do while loops in Java
 */
 public class DoWhileExample {
-public static void main(String[] args) {
-int num = 6;
-do {
-System.out.println("Number is: " + num);
-num++;
-}
-while (num <5); // Condition is false in the beginning.
-System.out.println("The value of count after the loop is " + num);
-}
+    public static void main(String[] args) {
+        int num = 6;
+
+        // Do-while loop
+        do {
+            System.out.println("Number is: " + num);
+            num++;
+        } while (num < 5); // Condition is false in the beginning.
+
+        // The following line will be executed after the loop.
+        System.out.println("The value of count after the loop is " + num);
+    }
 }
