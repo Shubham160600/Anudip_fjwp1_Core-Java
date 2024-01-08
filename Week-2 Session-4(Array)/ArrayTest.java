c8fffedd37e3c74d95d9d9f7e7079d4dc0f65044
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:38:05 +0530

    Update ArrayTest.java

--- Week-2 Session-4(Array)/ArrayTest.java
+++ Week-2 Session-4(Array)/ArrayTest.java
@@ -1,12 +1,15 @@
 package shubham;
 
 public class ArrayTest {
-public static void main(String[] args) {
-double [] myArr = new double [200];
-String [] messages = new String [15];
-System.out.println("The length of the double array = " + myArr.length);
-// As arrays is also an Object (Base class of all Java class)
-// we can invoke Object's method for an array
-System.out.println("The hashCode() of String array returned " + messages.hashCode());
-}
+    public static void main(String[] args) {
+        double[] myArr = new double[200];
+        String[] messages = new String[15];
+
+        // Print the length of the double array
+        System.out.println("The length of the double array = " + myArr.length);
+
+        // As arrays are also objects (Base class of all Java classes),
+        // we can invoke Object's method for an array
+        System.out.println("The hashCode() of String array returned " + messages.hashCode());
+    }
 }
