17cb73c633059285d669b87065f65ef5789c503a
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:31:15 +0530

    Update LoopInputExample.java

--- Week-2 Session-1 (Flow Control)/LoopInputExample.java
+++ Week-2 Session-1 (Flow Control)/LoopInputExample.java
@@ -1,14 +1,22 @@
 package shubham;
 
 import java.util.Scanner;
+
 public class LoopInputExample {
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-System.out.print("Enter five integers separated by spaces: ");
-for (int i = 0; i < 5; i++) {
-int num = scanner.nextInt();
-System.out.println("You entered: " + num);
-}
-scanner.close();
-}
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter five integers separated by spaces
+        System.out.print("Enter five integers separated by spaces: ");
+
+        // Use a for loop to read and print each entered number
+        for (int i = 0; i < 5; i++) {
+            int num = scanner.nextInt();
+            System.out.println("You entered: " + num);
+        }
+
+        // Close the Scanner to release system resources
+        scanner.close();
+    }
 }
