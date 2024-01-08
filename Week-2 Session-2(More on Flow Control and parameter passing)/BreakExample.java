5d5ec1a6fdd2ded3baa709315178655b702d7745
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:15 +0530

    Update BreakExample.java

--- Week-2 Session-2(More on Flow Control and parameter passing)/BreakExample.java
+++ Week-2 Session-2(More on Flow Control and parameter passing)/BreakExample.java
@@ -1,17 +1,40 @@
 package shubham;
 
 public class BreakExample {
-public static void main(String[] args) {
-//initiating while loop
-int a=1;
-while(a<=10){
-if(a==5){
-//using break statement
-a++;
-break;//it will break the while loop
-}
-System.out.println(a);
-a++;
-}
-}
+    public static void main(String[] args) {
+        public class BreakExample {
+            public static void main(String[] args) {
+                // Initializing a while loop with a counter 'a'
+                int a = 1;
+
+                // While loop to iterate from 1 to 10
+                while (a <= 10) {
+                    // Checking if the current value of 'a' is 5
+                    if (a == 5) {
+                        // Using the break statement to exit the loop when 'a' is 5
+                        a++;
+                        break; // It will break the while loop
+                    }
+
+                    // Printing the current value of 'a'
+                    System.out.println(a);
+
+                    // Incrementing the counter 'a'
+                    a++;
+                }
+            }
+        }
+
+        // initiating while loop
+        int a = 1;
+        while (a <= 10) {
+            if (a == 5) {
+                // using break statement
+                a++;
+                break;// it will break the while loop
+            }
+            System.out.println(a);
+            a++;
+        }
+    }
 }
