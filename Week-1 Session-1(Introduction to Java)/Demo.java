ef2d079bf76d56db32b1a92b8e94c0871cdb98a6
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 17:59:12 +0530

    Update Demo.java

--- Week-1 Session-1(Introduction to Java)/Demo.java
+++ Week-1 Session-1(Introduction to Java)/Demo.java
@@ -1,7 +1,7 @@
 package shubham;
 
 public class Demo {
-public static void main(String[] args) {
-System.out.println("Hello, we are learning Java!!!!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, we are learning Java!!!!");
+    }
 }
