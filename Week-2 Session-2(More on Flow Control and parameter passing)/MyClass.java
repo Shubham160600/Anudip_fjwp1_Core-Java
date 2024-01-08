f707fc1b047b2f0aff1afca6318e9603c282e642
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:22 +0530

    Update MyClass.java

--- Week-2 Session-2(More on Flow Control and parameter passing)/MyClass.java
+++ Week-2 Session-2(More on Flow Control and parameter passing)/MyClass.java
@@ -1,23 +1,29 @@
 package shubham;
 
 public class MyClass {
-private int x;
-private int y;
-// Instance initialization block
-{
-x = 10;
-y = 20;
-System.out.println("Instance initialization block executed.");
-}
-// Constructor
-public MyClass() {
-System.out.println("Constructor executed.");
-}
-public void display() {
-System.out.println("x: " + x + ", y: " + y);
-}
-public static void main(String[] args) {
-MyClass obj = new MyClass();
-obj.display();
-}
+    private int x;
+    private int y;
+
+    // Instance initialization block
+    {
+        x = 10;
+        y = 20;
+        System.out.println("Instance initialization block executed.");
+    }
+
+    // Constructor
+    public MyClass() {
+        System.out.println("Constructor executed.");
+    }
+
+    public void display() {
+        System.out.println("x: " + x + ", y: " + y);
+    }
+
+    public static void main(String[] args) {
+        // Creating an instance of MyClass
+        MyClass obj = new MyClass();
+        // Calling the display method
+        obj.display();
+    }
 }
