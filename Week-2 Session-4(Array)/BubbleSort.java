6af1c04cc115f847d2dab251a052a23f4b1a1857
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:38:02 +0530

    Update BubbleSort.java

--- Week-2 Session-4(Array)/BubbleSort.java
+++ Week-2 Session-4(Array)/BubbleSort.java
@@ -3,38 +3,36 @@ package shubham;
 //Bubble sort
 //Time Complexity : O(N^2)
 //Space Complexity : O(1)
-public class BubbleSort
-{
-static void bubbleSort(int a[])
-{
-int len = a.length;
-for (int i = 0; i < len-1; i++)
-for (int j = 0; j < len-i-1; j++)
-if (a[j] > a[j+1]) //comparing the pair of elements
-{
-//swapping a[j+1] and a[i]
-int temp = a[j];
-a[j] = a[j+1];
-a[j+1] = temp;
-}
-}
-/* Prints the array */
-static void printArray(int a[])
-{
-int len = a.length;
-for (int i = 0; i < len; i++)
-System.out.print(a[i] + " "); //print the sorted array
-System.out.println();
-}
-//Main method to test the sorting
-public static void main(String args[])
-{
-int arr[] = {7, 2, 9, 6, 4};
-System.out.println("Initial array is:");
-printArray(arr); //Calling the printArray function
-bubbleSort(arr);//Calling the bubbleSort function
-System.out.println("Sorted array is:");
-printArray(arr); //Calling the printArray function
-}
-}
 
+public class BubbleSort {
+    // Method to perform bubble sort on the array
+    static void bubbleSort(int a[]) {
+        int len = a.length;
+        for (int i = 0; i < len - 1; i++)
+            for (int j = 0; j < len - i - 1; j++)
+                if (a[j] > a[j + 1]) {
+                    // Swap a[j] and a[j + 1]
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+    }
+
+    // Method to print the elements of the array
+    static void printArray(int a[]) {
+        int len = a.length;
+        for (int i = 0; i < len; i++)
+            System.out.print(a[i] + " "); // Print the sorted array
+        System.out.println();
+    }
+
+    // Main method to test the sorting
+    public static void main(String args[]) {
+        int arr[] = { 7, 2, 9, 6, 4 };
+        System.out.println("Initial array is:");
+        printArray(arr); // Calling the printArray function
+        bubbleSort(arr); // Calling the bubbleSort function
+        System.out.println("Sorted array is:");
+        printArray(arr); // Calling the printArray function
+    }
+}
