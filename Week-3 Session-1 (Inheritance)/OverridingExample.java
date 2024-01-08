40601fb8cbba329d4a093791425abbdf57cd9a71
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 17:44:36 +0530

    Create OverridingExample.java

---
+++ Week-3 Session-1 (Inheritance)/OverridingExample.java
@@ -0,0 +1,28 @@
+/* Method overriding. */
+
+package shubham;
+
+class Animal {
+    // Basic implementation defined in the base class
+    public void makeSound() {
+        System.out.println("Animal is making sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        // Method with the same signature but a different implementation
+        System.out.println("Woof!"); // Different implementation
+    }
+}
+
+public class OverridingExample {
+    public static void main(String[] args) {
+        // Creating an instance of the Dog class
+        Dog dg = new Dog();
+
+        // Calling the overridden method in the Dog class
+        dg.makeSound();
+    }
+}
