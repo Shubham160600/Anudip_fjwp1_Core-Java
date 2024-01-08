9c406f2cfadd42c3e51978b7d86b54841596265b
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:48 +0530

    Update Balloon.java

--- Week-2 Session-2(More on Flow Control and parameter passing)/Balloon.java
+++ Week-2 Session-2(More on Flow Control and parameter passing)/Balloon.java
@@ -3,18 +3,22 @@ package shubham;
 public class Balloon {
     private String color;
 
-    public Balloon() {}
+    // Default constructor
+    public Balloon() {
+    }
 
+    // Parameterized constructor
     public Balloon(String c) {
         this.color = c;
     }
 
+    // Getter method for retrieving the color
     public String getColor() {
         return color;
     }
 
+    // Setter method for setting the color
     public void setColor(String color) {
         this.color = color;
     }
 }
-
