e9007c2a817ed4163a20b410c22301e9d2ef1a14
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:14:58 +0530

    Update SwitchCaseExample.java

--- Week-2 Session-1 (Flow Control)/SwitchCaseExample.java
+++ Week-2 Session-1 (Flow Control)/SwitchCaseExample.java
@@ -1,33 +1,38 @@
 package shubham;
 
 public class SwitchCaseExample {
-public static void main(String[] args) {
-int dayOfWeek = 3;
-switch (dayOfWeek) {
-case 1:
-System.out.println("Monday");
-break;
-case 2:
-System.out.println("Tuesday");
-break;
-case 3:
-System.out.println("Wednesday");
-break;
-case 4:
-System.out.println("Thursday");
-break;
-case 5:
-System.out.println("Friday");
-break;
-case 6:
-System.out.println("Saturday");
-break;
-case 7:
-System.out.println("Sunday");
-break;
-default:
-System.out.println("Invalid day");
-break;
-}
-}
+    public static void main(String[] args) {
+        // Assign a value to the dayOfWeek variable (for example, 3 represents
+        // Wednesday)
+        int dayOfWeek = 3;
+
+        // Switch statement to determine the day of the week based on the value of
+        // dayOfWeek
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
 }
