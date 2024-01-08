8fc48f187b996d5c6a6adf9e32c815cb91a18ff0
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:30:26 +0530

    Update PassingParamTest.java

--- Week-2 Session-2(More on Flow Control and parameter passing)/PassingParamTest.java
+++ Week-2 Session-2(More on Flow Control and parameter passing)/PassingParamTest.java
@@ -1,17 +1,21 @@
 package shubham;
 
-public class PassingParamTest{
-public static void main(String[] args) {
-int x = 1;
-int y = 2;
-// Before Modification
-System.out.println(x+" "+y); //1 2
-modify(x, y);
-// After Modification
-System.out.println(x+" "+y); //1 2
-}
-public static void modify(int x1, int y1) {
-x1 = 5;
-y1 = 10;
-}
+public class PassingParamTest {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+
+        // Before Modification
+        System.out.println(x + " " + y); // Output: 1 2
+
+        modify(x, y);
+
+        // After Modification
+        System.out.println(x + " " + y); // Output: 1 2
+    }
+
+    public static void modify(int x1, int y1) {
+        x1 = 5;
+        y1 = 10;
+    }
 }
