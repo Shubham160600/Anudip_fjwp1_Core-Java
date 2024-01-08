ed214dffe8c03c7527bb1989a1f627105699868b
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 18:02:15 +0530

    Update ArithMaticOperatorExamples.java

--- Week-1 Session-5(Java Operators)/ArithMaticOperatorExamples.java
+++ Week-1 Session-5(Java Operators)/ArithMaticOperatorExamples.java
@@ -1,18 +1,24 @@
 package abc;
 
 public class ArithMaticOperatorExamples {
-public static void main(String[] args) {
-int n1 = 12, n2 = 5;
-System.out.println("n1=" + n1 + ", n2=" + n2);
-// addition operator
-System.out.println("Addition: n1 + n2= " + (n1 + n2));
-// subtraction operator
-System.out.println("Subtraction: n1 - n2 = " + (n1 - n2));
-// multiplication operator
-System.out.println("Multiplication: n1 * n2 = " + (n1 * n2));
-// division operator
-System.out.println("Division: n1 / n2 = " + (n1 / n2));
-// modulo operator
-System.out.println("Modulo: n1 % n2 = " + (n1 % n2));
-}
+    public static void main(String[] args) {
+        int n1 = 12, n2 = 5;
+
+        System.out.println("n1=" + n1 + ", n2=" + n2);
+
+        // Addition operator
+        System.out.println("Addition: n1 + n2= " + (n1 + n2));
+
+        // Subtraction operator
+        System.out.println("Subtraction: n1 - n2 = " + (n1 - n2));
+
+        // Multiplication operator
+        System.out.println("Multiplication: n1 * n2 = " + (n1 * n2));
+
+        // Division operator
+        System.out.println("Division: n1 / n2 = " + (n1 / n2));
+
+        // Modulo operator
+        System.out.println("Modulo: n1 % n2 = " + (n1 % n2));
+    }
 }
