b8bee66f1c57a259ae2dd22d9c15d7c57d8d77c3
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 19:38:12 +0530

    Update ArraySorting.java

--- Week-2 Session-4(Array)/ArraySorting.java
+++ Week-2 Session-4(Array)/ArraySorting.java
@@ -1,17 +1,22 @@
 package shubham;
 
 import java.util.Arrays;
+
 public class ArraySorting {
-public static void main(String[] args) {
-//Declare and initialize an int array
-int [] arr = {56, 32, 12, 1 , 80, 2};
-// Arrays class in java's java.util package has a
-// sort() method that sort arrays of primitives and Objects
-Arrays.sort(arr);
-// Array arr is already sorted.
-System.out.println(Arrays.toString(arr)); // Print sorted array
-String [] currencies = {"INR", "USD", "GBP", "SGD"};
-Arrays.sort(currencies ); // Sorts alphabetically
-System.out.println(Arrays.toString(currencies));// Print alphabetically
-}
+    public static void main(String[] args) {
+        // Declare and initialize an int array
+        int[] arr = { 56, 32, 12, 1, 80, 2 };
+
+        // Arrays class in Java's java.util package has a
+        // sort() method that sorts arrays of primitives and Objects
+        Arrays.sort(arr);
+
+        // Array arr is already sorted.
+        System.out.println(Arrays.toString(arr)); // Print sorted array
+
+        String[] currencies = { "INR", "USD", "GBP", "SGD" };
+        Arrays.sort(currencies); // Sorts alphabetically
+
+        System.out.println(Arrays.toString(currencies));// Print alphabetically
+    }
 }
