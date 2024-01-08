c5a733085a661ef653058a6d090d0ec3e6cae840
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-08 17:58:40 +0530

    Update ExampleOfStatic.java

--- Week-1 Session-3(More about Classes)/ExampleOfStatic.java
+++ Week-1 Session-3(More about Classes)/ExampleOfStatic.java
@@ -1,23 +1,26 @@
 package saurabh;
 
 public class ExampleOfStatic {
-	// variables declared static
-	static String countryName = "India";
-	static String currency ="INR";
-	// method declared as static
-	static void display() {
-	System.out.println("Welcome!");
-	}
-	// non static field
-	int nonStaticFiled=20;
-	public static void main(String[] args) {
-	// Calling static fields and method by class name.
-	System.out.println (ExampleOfStatic.countryName);
-	System.out.println (ExampleOfStatic.currency);
-	ExampleOfStatic.display();
-	// Following line will produce compilation error
-	// because non static filed cannot be accessed
-	// by class name.
-	//System.out.println (ExampleOfStatic.nonStaticFiled);
-	}
-	}
+    // Static variables
+    static String countryName = "India";
+    static String currency = "INR";
+
+    // Static method
+    static void display() {
+        System.out.println("Welcome!");
+    }
+
+    // Non-static field
+    int nonStaticField = 20;
+
+    public static void main(String[] args) {
+        // Calling static fields and method using the class name
+        System.out.println(ExampleOfStatic.countryName);
+        System.out.println(ExampleOfStatic.currency);
+        ExampleOfStatic.display();
+
+        // The following line will produce a compilation error
+        // because a non-static field cannot be accessed by the class name.
+        // System.out.println(ExampleOfStatic.nonStaticField);
+    }
+}
