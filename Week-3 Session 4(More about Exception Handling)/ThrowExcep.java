5e4e6c17ca8462790cf3cdbf613e185b71c169f8
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-09 16:11:44 +0530

    Create ThrowExcep.java

---
+++ Week-3 Session 4(More about Exception Handling)/ThrowExcep.java
@@ -0,0 +1,23 @@
+/* throw and throws keywords.
+ * throw. */
+
+package shubham;
+
+class ThrowExcep {
+    static void fun() {
+        try {
+            throw new NullPointerException("demo");
+        } catch (NullPointerException e) {
+            System.out.println("Caught inside fun().");
+            throw e; // rethrowing the exception
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            fun();
+        } catch (NullPointerException e) {
+            System.out.println("Caught in main.");
+        }
+    }
+}
