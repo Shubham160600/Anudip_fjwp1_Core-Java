e18d3c22bfe57a3d60aa008aebb2fa534091f5a0
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-13 20:48:49 +0530

    Create ReverseListExample.java

---
+++ Week-4 Session-1(Collections)/Assignment3/ReverseListExample.java
@@ -0,0 +1,24 @@
+/* Write a program to reverse a given List of strings. */
+
+package shubham;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseListExample {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Apple");
+        stringList.add("Banana");
+        stringList.add("Orange");
+        stringList.add("Grapes");
+
+        System.out.println("Original List: " + stringList);
+
+        // Reverse the list using Collections.reverse()
+        Collections.reverse(stringList);
+
+        System.out.println("Reversed List: " + stringList);
+    }
+}
