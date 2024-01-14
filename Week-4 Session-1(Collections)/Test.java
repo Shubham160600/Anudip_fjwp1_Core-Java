9907018f00df0dcee13c848c14b6111ccdf152ec
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-14 19:11:56 +0530

    Create Test.java

---
+++ Week-4 Session-1(Collections)/Test.java
@@ -0,0 +1,71 @@
+/* Java program to Demonstrate List Interface. */
+
+package shubham;
+
+// Importing all utility classes
+import java.util.*;
+
+// Main class
+// Test class
+class Test {
+    // Main driver method
+    public static void main(String[] args) {
+        // Creating an object of List interface
+        // implemented by the ArrayList class
+        List<Integer> l1 = new ArrayList<Integer>();
+
+        // Adding elements to object of List interface
+        // Custom inputs
+        l1.add(0, 1);
+        l1.add(1, 2);
+
+        // Print the elements inside the object
+        System.out.println(l1);
+
+        // Now creating another object of the List
+        // interface implemented ArrayList class
+        // Declaring object of integer type
+        List<Integer> l2 = new ArrayList<Integer>();
+
+        // Again adding elements to object of List interface
+        // Custom inputs
+        l2.add(1);
+        l2.add(2);
+        l2.add(3);
+
+        // Will add list l2 from 1 index
+        l1.addAll(1, l2);
+        System.out.println(l1);
+
+        // Removes element from index 1
+        l1.remove(1);
+
+        // Printing the updated List 1
+        System.out.println(l1);
+
+        // Prints element at index 3 in list 1
+        // using get() method
+        System.out.println(l1.get(3));
+
+        // Replace 0th element with 5
+        // in List 1
+        l1.set(0, 5);
+
+        // Again printing the updated List 1
+        System.out.println(l1);
+
+        // sort the arraylist
+        List<String> listName = new ArrayList<String>();
+        listName.add("john");
+        listName.add("Steave");
+        listName.add("daniel");
+
+        Collections.sort(listName);
+        System.out.println("after sort-ascending order");
+        System.out.println(listName);
+
+        Collections.sort(listName, Collections.reverseOrder());
+        System.out.println("after sort-descending order");
+        System.out.println(listName);
+    }
+}
