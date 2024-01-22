37214aee39883ce6bdb9372823276ed688601713
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-22 09:18:18 +0530

    Create ControlStatementExample.java

---
+++ Week-2 Session-1 (Flow Control)/ControlStatementExample.java
@@ -0,0 +1,14 @@
+package shubham;
+
+public class ControlStatementExample {
+	public static void main(String args[]) {
+		// If Statement
+		
+		int age=20;
+		
+		if(age>=18)
+		System.out.println("You are eligible for vote.");
+	}
+}
+
+
