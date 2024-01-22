fba1db77a6e93dba07ae7971c40acb3af15c5a31
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-22 09:18:29 +0530

    Create MyProject.java

---
+++ Week-1 Session-3(More about Classes)/MyProject.java
@@ -0,0 +1,11 @@
+package shubham;
+
+public class MyProject {
+	static int a=10;
+	static {
+		System.out.println(a);
+	}
+	public static void main(String args[]) {
+		System.out.println("Main block");
+	}
+}
